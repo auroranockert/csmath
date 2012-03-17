450e19aa769796c7d81c73ffa07b2914061179c5
Jens Nockert <jens@nockert.se>
2012-03-17 14:02:26 +0100

    Yes, I fucked up, it did overwrite Math.sqrt instead of CSMath.sqrt

--- src/sqrt.js
+++ src/sqrt.js
@@ -3,31 +3,33 @@ if (CSMath.accurate['sqrt']) {
 } else {
 	/* Algorithm from 'Divide, Square Root and Remainder Algorithms for the IA64 Architecture */
 	
-	Math.sqrt = function (a) {
-		var y0 = 1.0 / Math.sqrta(a)
+	var sqrtApproximation = Math.sqrt
+	
+	CSMath.sqrt = function (a) {
+		var y0 = 1.0 / sqrtApproximation(a)
 
 		var H0 = 0.5 * y0,
 			S0 = (a * y0)
 
-		var d0 = -Math.fms(S0, H0, 0.5)
+		var d0 = -CSMath.fms(S0, H0, 0.5)
 	
-		var H1 = Math.fma(d0, H0, H0),
-			S1 = Math.fma(d0, S0, S0)
+		var H1 = CSMath.fma(d0, H0, H0),
+			S1 = CSMath.fma(d0, S0, S0)
 
-		var d1 = -Math.fms(S1, H1, 0.5)
+		var d1 = -CSMath.fms(S1, H1, 0.5)
 
-		var H2 = Math.fma(d1, H1, H1),
-			S2 = Math.fma(d1, S1, S1)
+		var H2 = CSMath.fma(d1, H1, H1),
+			S2 = CSMath.fma(d1, S1, S1)
 
-		var d2 = -Math.fms(S2, H2, 0.5),
-			e2 = -Math.fms(S2, S2, a)
+		var d2 = -CSMath.fms(S2, H2, 0.5),
+			e2 = -CSMath.fms(S2, S2, a)
 
-		var H3 = Math.fma(d2, H2, H2),
-			S3 = Math.fma(e2, H2, S2)
+		var H3 = CSMath.fma(d2, H2, H2),
+			S3 = CSMath.fma(e2, H2, S2)
 	
-		var e3 = -Math.fms(S3, S3, a)
+		var e3 = -CSMath.fms(S3, S3, a)
 
-		return Math.fma(e3, H3, S3)
+		return CSMath.fma(e3, H3, S3)
 	}
 }
 
